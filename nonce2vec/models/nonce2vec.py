Fixed buggy bugfix on exponential decay learning rate computation

nonce2vec/models/nonce2vec.py
@@ -81,7 +81,7 @@ def train_sg_pair(model, word, context_index, alpha,
         if learn_vectors:
             l1 += neu1e * lock_factor  # learn input -> hidden
                 # (mutates model.wv.syn0[word2.index], if that is l1)
-    return neu1e, alpha
+    return neu1e
 
 
 def train_batch_sg(model, sentences, alpha, work=None, compute_loss=False):
@@ -94,6 +94,7 @@ def train_batch_sg(model, sentences, alpha, work=None, compute_loss=False):
     nonce_count = 0
     for ctx_word, cwe in ctx_ent_tuples:
         ctx_vocab = model.wv.vocab[ctx_word]
+        nonce_count += 1
         if not model.train_with:
             raise Exception('Unspecified learning rate decay function. '
                             'You must specify a \'train_with\' parameter')
@@ -106,12 +107,9 @@ def train_batch_sg(model, sentences, alpha, work=None, compute_loss=False):
                                              round(cwe, 5),
                                              round(np.tanh(model.bias * cwe), 4),
                                              round(alpha, 5)))
-        train_sg_pair(model, model.wv.index2word[ctx_vocab.index],
-                      nonce_vocab.index, alpha, compute_loss=compute_loss)
-        nonce_count += 1
         if model.train_with == 'exp_alpha':
-            alpha = compute_exp_alpha(nonce_count, model.lambda_den, alpha,
-                                      model.min_alpha)
+            alpha = compute_exp_alpha(nonce_count, model.lambda_den,
+                                      model.alpha, model.min_alpha)
             logger.debug('training on \'{}\' and \'{}\' with cwe = {}, '
                          'alpha = {}'.format(model.wv.index2word[nonce_vocab.index],
                                              model.wv.index2word[ctx_vocab.index],
@@ -119,6 +117,8 @@ def train_batch_sg(model, sentences, alpha, work=None, compute_loss=False):
                                              round(alpha, 5)))
         if model.train_with == 'cst_alpha':
             alpha = model.alpha
+        train_sg_pair(model, model.wv.index2word[ctx_vocab.index],
+                      nonce_vocab.index, alpha, compute_loss=compute_loss)
         result += len(ctx_ent_tuples) + 1
     return result
 